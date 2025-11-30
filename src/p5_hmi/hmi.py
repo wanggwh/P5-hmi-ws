f52969a006684b3266138bb986a835458523c9ae
Gustav Wang <wang.gwh@gmail.com>
2025-11-30 20:21:01 +0100

    small fixes

--- src/p5_hmi/hmi.py
+++ src/p5_hmi/hmi.py
@@ -29,7 +29,7 @@ from pages.alice_configuration_setup import AliceConfigurationSetup
 from pages.mir_system_control import MirSystemControlPage
 from pages.admittance_control import AdmittanceControl
 from pages.dragon_drop import DragonDrop
-from pages.system_logging import SystemLoggingPage
+
 from pages.settings import SettingsPage
 from pages.status_popup_dialog import StatusPopupDialog
 from pages.error_msg_popup import ErrorMsgSnackbar
@@ -567,7 +567,6 @@ class HMIApp(MDApp):
             "MIR - System Control",
             "Admittance Control",
             "Drag and Drop - System Control",
-            "System Logging",
             "Settings"
         ]
 
@@ -607,7 +606,6 @@ class HMIApp(MDApp):
             "MIR - System Control": "kv/mir_system_control.kv",
             "Admittance Control": "kv/admittance_control.kv",
             "Drag and Drop - System Control": "kv/dragon_drop.kv",
-            "System Logging": "kv/system_logging.kv",
             "Settings": "kv/settings.kv"
         }
 
@@ -623,7 +621,7 @@ class HMIApp(MDApp):
         pages = [
             "Start Page", "BOB - Configuration Setup", "ALICE - Configuration Setup",
             "MIR - System Control", "Admittance Control", "Drag and Drop - System Control",
-            "System Logging", "Settings"
+            "Settings"
         ]
         
         for page in pages:
@@ -675,7 +673,6 @@ class HMIApp(MDApp):
                 "MIR - System Control": MirSystemControlPage,
                 "Admittance Control": AdmittanceControl,
                 "Drag and Drop - System Control": DragonDrop,
-                "System Logging": SystemLoggingPage,
                 "Settings": lambda: SettingsPage(self.hmi_node)
             }
 
--- src/p5_hmi/pages/system_logging.py
+++
@@ -1,6 +0,0 @@
-from kivymd.uix.boxlayout import MDBoxLayout
-
-
-class SystemLoggingPage(MDBoxLayout):
-    """System logging page with log viewer and controls"""
-    pass
