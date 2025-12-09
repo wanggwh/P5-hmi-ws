4ee483aee56243ef714dc0d0e81084bc136b771d
ROB5-Rasberry Pi <wang.gwh@gmail.com>
2025-12-09 15:33:39 +0000

    slsaf

--- make_hmi_container.sh
+++ make_hmi_container.sh
@@ -3,6 +3,6 @@ echo "Remember to run docker compose up alone the first time"
 docker compose up -d
 xhost +local:root
 sleep 0.1
-docker exec -it hmi-p5-p5_hmi-1 bash
+docker exec -it p5-hmi-ws-p5_hmi-1 bash
 source kivy_venv/bin/activate
 
